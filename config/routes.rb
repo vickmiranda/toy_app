bee07fe726cd0e68dad2e57edbb6144eadc2d540
vick <vickmiranda@hotmail.com>
2017-02-11 22:08:38 -0800

    Add scaffolds for users and microposts as per example

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
 	# change from get to root to have this
-	# as root => get 'greeter/hello' to:
-  root 'greeter#hello'
+	# as root => get 'greeter/hello' to: 'greeter#hello'
+  root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
